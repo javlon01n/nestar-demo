f1929c28663471d8dfaee5dbf34876d1ab91c85c
MARK <joxa030@gmail.com>
2026-09-12 19:30:49 +0900

    feat:  integration Header and Footer via Layout

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,8 +1,9 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { NextPage } from "next";
 import { useState } from "react";
 
-const Community = () => {
-    console.log("COMMUNITY COMPANENT - PAGES ROUTER");
-    
+const Community: NextPage = () => {
+  console.log("COMMUNITY COMPONENT - PAGES ROUTER");
   const [title, setTitle] = useState<string>("hello");
   return (
     <div>
@@ -14,4 +15,4 @@ const Community = () => {
   );
 };
 
-export default Community;
+export default withLayoutBasic(Community);
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -1,6 +1,14 @@
+import { Container, Stack } from "@mui/material";
+
 const PropertyList = () => {
-    console.log("PROPERTYLIST COMPANENT - PAGES ROUTER");
-    return <div>PROPERTY LIST</div>
+  console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
+  return (
+    <>
+      <Stack sx={{ background: "#81c784" }}>Header Basic</Stack>
+      <Container>PROPERTY LIST</Container>
+      <Stack sx={{ background: "#a1887f" }}>Footer</Stack>
+    </>
+  );
 };
 
 export default PropertyList;
